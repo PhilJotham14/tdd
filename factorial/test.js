072a062f92205c0f91b178d0052443f2d076a804
Phil <ajeinstein3@gmail.com>
2021-02-08 15:01:47 +0300

    factorial_test

---
+++ factorial/test.js
@@ -0,0 +1,24 @@
+  
+const assert = require('assert');
+const factorial = require('./app');
+
+
+describe ('factorial', ()=>{
+    
+    it('factorial of 3 is 6', () =>{
+        assert.equal(6, factorial(3));
+   
+})
+it('factorial of 1 is 1', () =>{
+    assert.equal(1, factorial(1)); 
+
+})
+it('factorial of 0 is 1', () =>{
+    assert.equal(15, factorial(0)); 
+
+})
+it('factorial of 5 is 120', () =>{
+    assert.equal(120, factorial(5)); 
+
+})
+})
