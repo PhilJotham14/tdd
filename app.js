ba676e17da8239113b3d569a2f9e4031ab7b7a1c
Phil <ajeinstein3@gmail.com>
2021-02-08 14:48:32 +0300

    fibonacci_test

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+---------------------------....GROUP MEMBERS....-----------------------
+>>>>>>>>>>>>>>>> STEPHEN MWANIKA
+>>>>>>>>>>>>>>>> OKIROR PHILLIP JOTHAM
+*/
+
+/*
+  Fibonacci numbers are numbers obtained by adding the two previous numbers in the sequence to obtain the next higher number.
+  Example: 1+1= 2, 2+3= 5, 5+8= 13...
+  The formula is: Fn = Fn-1 + Fn-2
+*/
+  function fibonacci(number) {
+    if (number <= 1) 
+      return 1;
+    
+  // Next fibonacci number = previous + one before previous
+    return fibonacci(number - 1) + fibonacci(number - 2);
+  }
+  console.log(fibonacci(1));
+
+  module.exports = fibonacci;
